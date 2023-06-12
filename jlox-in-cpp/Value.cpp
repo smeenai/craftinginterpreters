d09ea64aeda25ffed7bfe09be4bcb9c9fdbfad3b
Shoaib Meenai <shoaib.meenai@gmail.com>
2023-06-12 00:11:20 -0700

    [jlox-in-cpp] Add ref-counted StringValue
    It's okay to keep tokens and AST nodes allocated for the lifetime of the
    interpreter, since they're bounded by the input program size, but now
    that we have loops, strings generated at runtime through concatenation
    can grow unbounded, and keeping them around permanently is potentially
    very inefficient. Add a simple ref-counted StringValue type instead so
    that strings are deallocated when they become unused.
    
    With the following test program:
    
        var s = "s";
        for (var i = 0; i < 100000; i = i + 1)
          s = s + "s";
    
    With the previous implementation, it took 0.9 seconds and used 5.2 GB of
    memory. It now takes 0.1 seconds and uses 1.9 MB of memory.
    
    Adding other assignments afterwards (e.g. `var s2 = s`) doesn't increase
    memory usage, confirming the ref-counting is working as intended. I also
    printed out some (smaller) strings to verify their contents.
    
    If I add one more zero:
    
        var s = "s";
        for (var i = 0; i < 1000000; i = i + 1)
          s = s + "s";
    
    The interpreter takes 9.6 seconds and 16.7 MB of memory, so the time
    growth isn't linear but the memory growth is. Interestingly enough, if I
    convert the same program to C++:
    
        #include <string>
        std::string test() {
          std::string s;
          for (int i = 0; i < 1000000; i = i + 1)
            s = s + "s";
          return s;
        }
    
    And compile it with -O3 (with some separate compilation tricks to make
    sure the compiler doesn't throw away the whole loop), it takes 10.3
    seconds and uses 15.7 MB of memory, so we're basically on par here. (If
    you change the loop to a += then the runtime goes down to 0.2 seconds
    and the memory usage to 3.7 MB, but that's less fun.)

--- jlox-in-cpp/Value.cpp
+++ jlox-in-cpp/Value.cpp
@@ -3,7 +3,7 @@
 std::ostream &operator<<(std::ostream &o, Value value) {
   struct {
     void operator()(double d) { o << d; }
-    void operator()(std::string_view s) { o << s; }
+    void operator()(StringValue s) { o << s.str(); }
     void operator()(bool b) { o << (b ? "true" : "false"); }
     void operator()(std::nullptr_t) { o << "nil"; }
     std::ostream &o;
--- jlox-in-cpp/Value.h
+++ jlox-in-cpp/Value.h
@@ -1,10 +1,87 @@
 #pragma once
 
+#include <cstring>
+#include <limits>
 #include <ostream>
+#include <string_view>
+#include <utility>
 #include <variant>
 
-// This is equivalent to the LiteralExpr variant right now, but I suspect it
-// won't be after subsequent chapters, so I'm keeping it separate.
-using Value = std::variant<std::nullptr_t, bool, double, std::string_view>;
+// This is a wrapper around a string view which is either unowned (someone else
+// manages its lifetime) or reference counted. It also implements string
+// concatenation, since we need that operation for our interpreter. The
+// motivation is to avoid holding on to strings that didn't directly come from
+// the parsed program (e.g. a new string we created via concatenation) when
+// they're unneeded, to reduce memory usage. We aren't using shared_ptr because
+// handling both the unowned and the ref-counted case would have been more work
+// with that IMO, since I don't need atomicity or weak pointers.
+class StringValue {
+public:
+  StringValue(std::string_view s) : data(s.data()), ownsData(false) {
+    assert(s.length() <= std::numeric_limits<unsigned>::max());
+    length = static_cast<unsigned>(s.length());
+  }
+
+  StringValue(const StringValue &s1, const StringValue &s2) {
+    size_t combinedLength = s1.length + s2.length;
+    assert(combinedLength >= s1.length); // check for overflow
+
+    char *data = new char[sizeof(unsigned) + combinedLength];
+    ownsData = true;
+    std::memcpy(data + sizeof(unsigned), s1.data, s1.length);
+    std::memcpy(data + sizeof(unsigned) + s1.length, s2.data, s2.length);
+    length = combinedLength;
+
+    // Point to string
+    this->data = data + sizeof(unsigned);
+    refCount() = 1;
+  }
+
+  StringValue(const StringValue &other)
+      : data(other.data), length(other.length), ownsData(other.ownsData) {
+    if (ownsData)
+      ++refCount();
+  }
+
+  StringValue(StringValue &&other)
+      : data(other.data), length(other.length), ownsData(other.ownsData) {
+    other.ownsData = false;
+  }
+
+  StringValue &operator=(StringValue other) {
+    // https://stackoverflow.com/a/3279550
+    swap(*this, other);
+    return *this;
+  }
+
+  ~StringValue() {
+    if (ownsData && --refCount() == 0)
+      delete[] (data - sizeof(unsigned));
+  }
+
+  bool operator==(const StringValue &other) const {
+    return str() == other.str();
+  }
+
+  std::string_view str() const { return std::string_view(data, length); }
+
+private:
+  const char *data;
+  unsigned length;
+  bool ownsData;
+
+  unsigned &refCount() {
+    return *reinterpret_cast<unsigned *>(
+        const_cast<char *>(data - sizeof(unsigned)));
+  }
+
+  friend void swap(StringValue &first, StringValue &second) {
+    std::swap(first.data, second.data);
+    std::swap(first.length, second.length);
+    std::swap(first.ownsData, second.ownsData);
+  }
+};
+
+using Value = std::variant<std::nullptr_t, bool, double, StringValue>;
 
 std::ostream &operator<<(std::ostream &o, Value value);
