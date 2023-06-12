5c84ca89804de615d249fe91f0dcf0c4d377fc8c
Shoaib Meenai <shoaib.meenai@gmail.com>
2023-06-12 00:11:20 -0700

    [jlox-in-cpp] Make environment store string_view
    Now that our inputs are guaranteed to last the lifetime of the
    interpreter even in REPL mode, we can just use string_view.

--- jlox-in-cpp/Environment.h
+++ jlox-in-cpp/Environment.h
@@ -19,9 +19,7 @@ public:
   // I'm following the book and taking String instead of Token like get below
   // (which needs Token so it can construct a RuntimeError). I imagine there's a
   // reason for this which will be revealed later.
-  void define(std::string_view name, Value value) {
-    values[std::string(name)] = value;
-  }
+  void define(std::string_view name, Value value) { values[name] = value; }
 
   Value get(const Token &name) const {
     for (const Environment *env = this; env != nullptr;
@@ -57,29 +55,11 @@ public:
   }
 
   void assignAt(unsigned distance, const Token &name, Value value) {
-    ancestor(distance).values[std::string(name.lexeme)] = value;
+    ancestor(distance).values[name.lexeme] = value;
   }
 
 private:
-  // This allows heterogenous lookup so we don't have to construct a std::string
-  // to look up names: https://stackoverflow.com/a/53530846
-  struct string_view_hash {
-    using is_transparent = void;
-    std::size_t operator()(std::string_view str) const {
-      return std::hash<std::string_view>()(str);
-    }
-  };
-
-  // Right now in Lox.cpp, the interpreter is file-static but REPL input lines
-  // (which contain the variable names) are scoped to the loop in runPrompt, so
-  // holding a string_view here would lead to dangling references. We could keep
-  // the entire input line alive, but that would use more memory than just
-  // copying the variable names over. We'll do all our lookups using string_view
-  // to avoid any unnecessary copying there.
-  // TODO: Reconsider this if it turns out we need to keep the entire REPL input
-  // lines alive for some other reason anyway.
-  std::unordered_map<std::string, Value, string_view_hash, std::equal_to<>>
-      values;
+  std::unordered_map<std::string_view, Value> values;
 
   std::shared_ptr<Environment> enclosing;
 
