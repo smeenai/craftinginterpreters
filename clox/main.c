08df1477c87369128425f6eb704889c379c84e4b
Shoaib Meenai <shoaib.meenai@gmail.com>
2023-07-01 09:28:46 -0700

    [WIP][clox] Scanning
    Doesn't compile, just syncing across machines.

--- clox/main.c
+++ clox/main.c
@@ -1,41 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sysexits.h>
+
 #include "chunk.h"
 #include "common.h"
 #include "debug.h"
 #include "vm.h"
 
-int main(int argc, const char *argv[]) {
-  (void)argc;
-  (void)argv;
+static void repl() {
+  char line[1024];
+  while (true) {
+    printf("> ");
 
+    if (!fgets(line, sizeof(line), stdin)) {
+      puts("");
+      break;
+    }
+
+    interpret(line);
+  }
+}
+
+static void runFile(const char *path) {
+  char *source = readFile(path);
+  InterpretResult result = interpret(source);
+  free(source);
+
+  if (result == INTERPRET_COMPILE_ERROR)
+    exit(EX_DATAERR);
+  if (result == INTERPRET_RUNTIME_ERROR)
+    exit(EX_SOFTWARE);
+}
+
+int main(int argc, const char *argv[]) {
   initVM();
 
-  Chunk chunk;
-  initChunk(&chunk);
-
-  unsigned constant = addConstant(&chunk, 1.2);
-  writeChunk(&chunk, OP_CONSTANT, 123);
-  writeChunk(&chunk, constant, 123);
-
-  constant = addConstant(&chunk, 3.4);
-  writeChunk(&chunk, OP_CONSTANT, 123);
-  writeChunk(&chunk, constant, 123);
-
-  writeChunk(&chunk, OP_ADD, 123);
-
-  constant = addConstant(&chunk, 5.6);
-  writeChunk(&chunk, OP_CONSTANT, 123);
-  writeChunk(&chunk, constant, 123);
-
-  writeChunk(&chunk, OP_DIVIDE, 123);
-  writeChunk(&chunk, OP_NEGATE, 123);
-
-  writeChunk(&chunk, OP_RETURN, 123);
-
-  disassembleChunk(&chunk, "test chunk");
-  interpret(&chunk);
-  freeChunk(&chunk);
+  if (argc == 1) {
+    repl();
+  } else if (argc == 2) {
+    runFile(argv[1]);
+  } else {
+    fputs("Usage: clox[path]\n", stderr);
+    exit(EX_USAGE);
+  }
 
   freeVM();
-
   return 0;
 }
